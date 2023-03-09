62de835f21a1b697c47b9d07b21691f695f37f96
khomkolov_igor <khomkolov99@gmail.com>
2023-03-09 13:51:30 +0700

    JS edit

---
+++ frontend/src/component/Chat.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Sidebar from "./Sidebar";
+
+function Chat () {
+  return (
+    <div className ="chat">
+        <Sidebar />
+    </div>
+  )
+};
+
+export default Chat;
---
+++ frontend/src/component/SidebarThread.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function SidebarThread () {
+  return (
+    <div className ="sidebarThread">
+        sidebarthr
+    </div>
+  )
+};
+
+export default SidebarThread;
